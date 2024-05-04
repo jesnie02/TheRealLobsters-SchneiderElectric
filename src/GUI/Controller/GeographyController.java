0c869d25089b2710ad515b98274d24a443c68694
Jesper Nielsen <jesnie02@easv365.dk>
2024-05-04 13:02:43 +0200

    flyttet Geography over i dashboard og tilføjet owerview

--- src/GUI/Controller/GeographyController.java
+++ src/GUI/Controller/GeographyController.java
@@ -1,47 +1,7 @@
 package GUI.Controller;
 
-import BE.Country;
-import BE.Geography;
-import BE.ProjectTeam;
-import GUI.Model.CountryModel;
-import GUI.Model.GeographyModel;
-import GUI.Model.ProjectTeamsModel;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class GeographyController implements Initializable {
-    @FXML
-    private ComboBox<Country> cBoxCountryGeo;
-    @FXML
-    private ComboBox<Geography> cBoxRegionGeo;
-    @FXML
-    private ComboBox<ProjectTeam> cBoxTeamGeo;
-
-    private CountryModel countryModel;
-    private ProjectTeamsModel projectTeamsModel;
-    private GeographyModel geographyModel;
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        try {
-            countryModel = new CountryModel();
-            projectTeamsModel = new ProjectTeamsModel();
-            geographyModel = new GeographyModel();
+public class GeographyController  {
 
-            cBoxCountryGeo.setItems(countryModel.getAllFromCountries());
-
-            //cBoxRegionGeo.setItems(geographyModel.getAllFromGeographies());
-        } catch (IOException e) {
-            e.printStackTrace(); //TODO: Handle this exception
-        } catch (Exception e) {
-            throw new RuntimeException(e); //TODO: Handle this exception
-        }
-    }
 }
