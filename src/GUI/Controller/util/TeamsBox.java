293576d35f4e564a4121e43c4f03a13e43ee95b7
Thomas Olesen <thomasolesen0505@hotmail.com>
2024-05-04 20:59:35 +0200

    ændret navngivning til TeamBox

---
+++ src/GUI/Controller/util/TeamsBox.java
@@ -0,0 +1,50 @@
+package GUI.Controller.util;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TeamsBox {
+    @FXML
+    private Label lblLocation;
+    @FXML
+    private Label lblGeography;
+    @FXML
+    private Label lblNumberOfMembers;
+    @FXML
+    private Label lblAnnualAVGTeamCost;
+
+
+
+
+    private void setLblLocation(Label lblLocation) {
+
+    }
+
+
+    private void setLblGeography(Label lblGeography) {
+        this.lblGeography = lblGeography;
+    }
+
+
+    private void setLblNumberOfMembers(Label lblNumberOfMembers) {
+        this.lblNumberOfMembers = lblNumberOfMembers;
+    }
+
+
+
+    private void setLblAnnualAVGTeamCost(Label lblAnnualAVGTeamCost) {
+        this.lblAnnualAVGTeamCost = lblAnnualAVGTeamCost;
+    }
+
+    @FXML
+    public void initialize() {
+        setLblLocation(lblLocation);
+        setLblGeography(lblGeography);
+        setLblNumberOfMembers(lblNumberOfMembers);
+        setLblAnnualAVGTeamCost(lblAnnualAVGTeamCost);
+    }
+}
