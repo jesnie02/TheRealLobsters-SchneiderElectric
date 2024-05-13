a0d0ade94c0d1e20d538484ea4b3a44ca81cf0e8
MadsFM <madsmoeller13@gmail.com>
2024-05-13 21:43:14 +0200

    fixed load af valuta fil

--- src/GUI/Controller/util/CurrencyReader.java
+++ src/GUI/Controller/util/CurrencyReader.java
@@ -1,15 +1,15 @@
 package GUI.Controller.util;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CurrencyReader {
+
     public static Map<String, String> readCurrencyFromFile(String filePath) {
         Map<String, String> currencyMap = new HashMap<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        try ( InputStream inputStream = CurrencyReader.class.getResourceAsStream("/Currency");
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(" - ");
