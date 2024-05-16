e1926fc75b66ddb16f6ad92ad0090f93fe3af9be
Jesper Nielsen <jesnie02@easv365.dk>
2024-05-16 13:34:30 +0200

    slider i create profile vises i tekstfelt

--- src/GUI/Controller/CreateProfileController.java
+++ src/GUI/Controller/CreateProfileController.java
@@ -45,7 +45,7 @@ public class CreateProfileController implements Initializable {
 
     @FXML
     private Label lblHourlyResult, lblDailyResult, lblShowMassage;
-
+    private double overheadMultiplierProfile;
     // comboBox for country and team
 
 
@@ -69,7 +69,7 @@ public class CreateProfileController implements Initializable {
             profileModel = new ProfileModel();
             profileRoleModel = new ProfileRoleModel();
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); //TODO: Handle this exception
         }
     }
 
@@ -86,6 +86,8 @@ public class CreateProfileController implements Initializable {
         setupComboBoxCustomization();
         setupRegex();
         populateProfileRolesComboBox();
+
+
     }
 
     private void populateProfileRolesComboBox() {
@@ -148,8 +150,13 @@ public class CreateProfileController implements Initializable {
      */
     public void listenersOnSliders() {
         sliderOverhead.valueProperty().addListener((observable, oldValue, newValue) -> {
-            txtOverheadView.setText(String.format("%.2f", newValue));
+            overheadMultiplierProfile = newValue.doubleValue();
+            setTextInField();
         });
+
+    }
+    private void setTextInField(){
+        txtOverheadView.setText(String.valueOf(overheadMultiplierProfile));
     }
 
     /**
